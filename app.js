9756175f95ee8525e22959754f74db13fd9dc672
www <l2u0k0a0s.ld@gmail.com>
2022-02-06 05:25:54 +0100

    pre-release-0.0.1

---
+++ app.js
@@ -0,0 +1,10 @@
+const SteamUser = require('steam-user');
+const GlobalOffensive = require('globaloffensive');
+
+let steam = new SteamUser();
+let csgo = new GlobalOffensive(steam);
+
+(async () => {
+	// Login to an given steam account
+    
+})();
